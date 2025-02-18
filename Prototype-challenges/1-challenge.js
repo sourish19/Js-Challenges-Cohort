a9df1172539b71304fca5898376db88923db2eed
Sourish Dey <sourish19z@gmail.com>
2025-02-19 00:52:16 +0530

    Update 1-challenge.js

--- Prototype-challenges/1-challenge.js
+++ Prototype-challenges/1-challenge.js
@@ -1,11 +1,9 @@
 function Animal(name) {
-  // this.name = name;
+  this.name = name;
   Animal.prototype.makesound = function () {
     return "Some generic sound";
   };
 }
 
 const animal = new Animal("Leo");
-console.log(typeof animal);
-// console.log(animal.name);
 console.log(animal.makesound());
